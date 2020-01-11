aa6302e2e57fe20f79b4f310f5aae23ef381ca07
shuheydev <shuhey.dev@gmail.com>
2020-01-11 11:47:31 +0900

    Refactored using Switch expression.

--- CustomRenderer/CustomRenderer.Android/Renderers/GradientStackLayoutRenderer.cs
+++ CustomRenderer/CustomRenderer.Android/Renderers/GradientStackLayoutRenderer.cs
@@ -21,188 +21,7 @@ namespace CustomRenderer.Droid.Renderers
             var hd = Element.GradientHorizontalDirection;
             var vd = Element.GradientVerticalDirection;
 
-            LinearGradient gradient = null;
-
-            if(hd==GradientHorizontalDirection.LeftToRight && vd==GradientVerticalDirection.TopToBottom)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: 0,
-                        y0: 0,
-                        x1: Width * Element.GradientRatio,
-                        y1: Height * Element.GradientRatio,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.LeftToRight && vd == GradientVerticalDirection.BottomToTop)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: 0,
-                        y0: Height * Element.GradientRatio,
-                        x1: Width * Element.GradientRatio,
-                        y1: 0,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.LeftToRight && vd == GradientVerticalDirection.Nothing)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: 0,
-                        y0: 0,
-                        x1: Width * Element.GradientRatio,
-                        y1: 0,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.RightToLeft && vd == GradientVerticalDirection.TopToBottom)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: Width * Element.GradientRatio,
-                        y0: 0,
-                        x1: 0,
-                        y1: Height * Element.GradientRatio,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.RightToLeft && vd == GradientVerticalDirection.BottomToTop)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: Width * Element.GradientRatio,
-                        y0: Height * Element.GradientRatio,
-                        x1: 0,
-                        y1: 0,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.RightToLeft && vd == GradientVerticalDirection.Nothing)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: Width * Element.GradientRatio,
-                        y0: 0,
-                        x1: 0,
-                        y1: 0,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.Nothing && vd == GradientVerticalDirection.TopToBottom)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: 0,
-                        y0: 0,
-                        x1: 0,
-                        y1: Height * Element.GradientRatio,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.Nothing && vd == GradientVerticalDirection.BottomToTop)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: 0,
-                        y0: Height * Element.GradientRatio,
-                        x1: 0,
-                        y1: 0,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.EndColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
-            if (hd == GradientHorizontalDirection.Nothing && vd == GradientVerticalDirection.Nothing)
-            {
-                //X軸始点
-                //Y軸始点
-                //X軸終点
-                //Y軸終点
-                //開始色
-                //終了色
-                //範囲外の描画方法
-                gradient = new LinearGradient
-                    (
-                        x0: 0,
-                        y0: 0,
-                        x1: 0,
-                        y1: 0,
-                        color0: Element.StartColor.ToAndroid(),
-                        color1: Element.StartColor.ToAndroid(),
-                        tile: Shader.TileMode.Clamp
-                    );
-            }
+            LinearGradient gradient = ComposeGradient(hd, vd);
 
             Paint paint = new Paint
             {
@@ -212,6 +31,56 @@ namespace CustomRenderer.Droid.Renderers
             canvas.DrawPaint(paint);
 
             base.DispatchDraw(canvas);
+        }
+
+        private LinearGradient ComposeGradient(GradientHorizontalDirection hd, GradientVerticalDirection vd)
+        {
+            #region グラデーションの方向
+            float width = Width * Element.GradientRatio;
+            float height = Height * Element.GradientRatio;
+
+            //Switch式便利!
+            (float x0, float x1) = hd switch
+            {
+                GradientHorizontalDirection.LeftToRight => (0f, width),
+                GradientHorizontalDirection.RightToLeft => (width, 0f),
+                _ => (0f, 0f)
+            };
+            (float y0, float y1) = vd switch
+            {
+                GradientVerticalDirection.TopToBottom => (0f, height),
+                GradientVerticalDirection.BottomToTop => (height, 0f),
+                _ => (0f, 0f)
+            };
+            #endregion
+
+
+            var color0 = Element.StartColor.ToAndroid();
+            var color1 = Element.EndColor.ToAndroid();
+            //縦横双方がNothingだった場合は,StartColor一色で塗りつぶすようにする
+            if (hd == GradientHorizontalDirection.Nothing && vd == GradientVerticalDirection.Nothing)
+            {
+                color1 = color0;
+            }
+
+            //X軸始点
+            //Y軸始点
+            //X軸終点
+            //Y軸終点
+            //開始色
+            //終了色
+            //範囲外の描画方法
+            LinearGradient gradient = new LinearGradient(
+                        x0: x0,
+                        y0: y0,
+                        x1: x1,
+                        y1: y1,
+                        color0: color0,
+                        color1: color1,
+                        tile: Shader.TileMode.Clamp
+                );
+
+            return gradient;
         }
     }
 }
