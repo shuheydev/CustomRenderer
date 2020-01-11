44b70d4741382e8d5df9b989e1775536cd5f12d8
shuheydev <shuhey.dev@gmail.com>
2020-01-12 02:45:14 +0900

    Add drop shadow frame and create drop shadow button using drop shadow frame.

---
+++ CustomRenderer/CustomRenderer/Controlls/ShadowFrame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CustomRenderer.Controlls
+{
+    public class ShadowFrame:Frame
+    {
+
+        public float Elevation
+        {
+            get { return (float)GetValue(ElevationProperty); }
+            set { SetValue(ElevationProperty, value); }
+        }
+        public static readonly BindableProperty ElevationProperty = BindableProperty.Create(
+               propertyName: nameof(Elevation),
+               returnType: typeof(float),
+               declaringType: typeof(ShadowFrame),
+               defaultValue: 4.0f,
+               defaultBindingMode: BindingMode.OneWay,
+               propertyChanged: ElevationPropertyChanged
+            );
+        private static void ElevationPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+        }
+    }
+}
