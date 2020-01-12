e72c8aa9d22619e678b5d23a014d118339f9bdae
shuheydev <shuhey.dev@gmail.com>
2020-01-12 18:52:03 +0900

    clean up.

--- CustomRenderer/CustomRenderer/Controlls/ShadowFrame.cs
+++ CustomRenderer/CustomRenderer/Controlls/ShadowFrame.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace CustomRenderer.Controlls
 {
-    public class ShadowFrame:Frame
+    public class ShadowFrame : Frame
     {
-
         public float Elevation
         {
             get { return (float)GetValue(ElevationProperty); }
